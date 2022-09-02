40d2819b4e42704192a2e1ddd1466efaf479dea7
Jerry <jerlee.evans@gmail.com>
2022-09-02 20:55:48 +0200

    its second commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// alert('Hello MotherFucker');
+
+// const result = confirm("are you here");
+// console.log(result);
+
+// console.log(typeof(answer));
+// console.log(typeof(answer));
+// const answers = [];
+// console.log(typeof(answers));
+
+
+// answers[0] = prompt('Wo tu loh?', '');
+// answers[1] = prompt('Kak dela?', '');
+// answers[5] = prompt('gde tu!', '');
+// console.log(typeof(answers));
+
+// const answer = +prompt("введите число", "18");
+// console.log(answer + 20);
+
+// const category = 'toys';
+
+// console.log(`https//someurl.com/$(category)/5`);
+
+// const user = "Ivan";
+
+// alert(`Ky, ${user}`);
+
+console.log('arr' + " FUCK u");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log(incr);
+    console.log(decr);
+
+    const isChecked = false,
+          isClose = false;
+
+          console.log(isChecked || isClose);
+
